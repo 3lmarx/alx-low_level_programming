eab8d9c491879376152445b857993fc4ff10c061
Lesedi <marcussibeko3@gmail.com>
2023-02-16 07:35:38 -0800

    yes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-  int my_num;
-  float my_float;
-  long int mydouble;
-  long long int mylng;
-  char mystring;
+int my_num;
+float my_float;
+long int mydouble;
+long long int mylng;
+char mystring;
 
-  //sizeof opperator evaluates the size of the types
-  printf("size of int: %lu byte(s)\n",sizeof(my_num));
-  printf("size of float: %lu byte(s)\n",sizeof(my_float));
-  printf("size of long int: %lu byte(s)\n",sizeof(mydouble));
-  printf("size of char: %lu byte(s)\n",sizeof(mystring));
-  printf("size of long long int: %lu byte(s)\n",sizeof(mylng));
-  return 0;
+printf("size of int: %lu byte(s)\n", sizeof(my_num));
+printf("size of float: %lu byte(s)\n", sizeof(my_float));
+printf("size of long int: %lu byte(s)\n", sizeof(mydouble));
+printf("size of char: %lu byte(s)\n", sizeof(mystring));
+printf("size of long long int: %lu byte(s)\n", sizeof(mylng));
+
+return (0);
 }
