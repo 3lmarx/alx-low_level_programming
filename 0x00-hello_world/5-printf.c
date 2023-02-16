12d8bcba8e0339158ea3e39dd1b111d9dc383a5d
Lesedi <marcussibeko3@gmail.com>
2023-02-16 10:22:05 -0800

    finally

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 printf("with proper grammer, but the outcome is a piece of art,\n");
