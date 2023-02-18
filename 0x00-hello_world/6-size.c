189b8201580997c203446d96b7a0b03474f118c6
Lesedi <marcussibeko3@gmail.com>
2023-02-18 05:32:43 -0800

    finally man

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int my_num;
 long int mydouble;
 long long int mylng;
 float my_float;
-printf("size of char: %lu byte(s)\n", sizeof(mystring));
-printf("size of int: %lu byte(s)\n", sizeof(my_num));
-printf("size of long int: %lu byte(s)\n", sizeof(mydouble));
-printf("size of long long int: %lu byte(s)\n", sizeof(mylng));
-printf("size of float: %lu byte(s)\n", sizeof(my_float));
+printf("size of a char: %lu byte(s)\n", sizeof(mystring));
+printf("size of an int: %lu byte(s)\n", sizeof(my_num));
+printf("size of a long int: %lu byte(s)\n", sizeof(mydouble));
+printf("size of a long long int: %lu byte(s)\n", sizeof(mylng));
+printf("size of a float: %lu byte(s)\n", sizeof(my_float));
 return (0);
 }
