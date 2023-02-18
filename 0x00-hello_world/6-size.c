4f274dc761a0540b37428a4c5efcb1621b675e9f
Lesedi <marcussibeko3@gmail.com>
2023-02-18 05:26:42 -0800

    my programs

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,21 +4,20 @@
  *main - Entry point
  *
  *Return: Always 0 (Success)
+ *
  */
 
 int main(void)
 {
+char mystring;
 int my_num;
-float my_float;
 long int mydouble;
 long long int mylng;
-char mystring;
-
-printf("size of int: %lu byte(s)\n", sizeof(my_num));
-printf("size of float: %lu byte(s)\n", sizeof(my_float));
-printf("size of long int: %lu byte(s)\n", sizeof(mydouble));
+float my_float;
 printf("size of char: %lu byte(s)\n", sizeof(mystring));
+printf("size of int: %lu byte(s)\n", sizeof(my_num));
+printf("size of long int: %lu byte(s)\n", sizeof(mydouble));
 printf("size of long long int: %lu byte(s)\n", sizeof(mylng));
-
+printf("size of float: %lu byte(s)\n", sizeof(my_float));
 return (0);
 }
