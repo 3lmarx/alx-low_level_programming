edd920d4d3543714572596339c4045fdbb3c5604
Lesedi <marcussibeko3@gmail.com>
2023-02-18 05:36:31 -0800

    so many commits but we there

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int my_num;
 long int mydouble;
 long long int mylng;
 float my_float;
-printf("size of a char: %lu byte(s)\n", sizeof(mystring));
-printf("size of an int: %lu byte(s)\n", sizeof(my_num));
-printf("size of a long int: %lu byte(s)\n", sizeof(mydouble));
-printf("size of a long long int: %lu byte(s)\n", sizeof(mylng));
-printf("size of a float: %lu byte(s)\n", sizeof(my_float));
+printf("Size of a char: %lu byte(s)\n", sizeof(mystring));
+printf("Size of an int: %lu byte(s)\n", sizeof(my_num));
+printf("Size of a long int: %lu byte(s)\n", sizeof(mydouble));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(mylng));
+printf("Size of a float: %lu byte(s)\n", sizeof(my_float));
 return (0);
 }
